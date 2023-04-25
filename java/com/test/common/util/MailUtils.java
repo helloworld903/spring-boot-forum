05a4ceb4bd8bfd9a15c302c526331f9cbf37cd23
helloworld903 <80182812+helloworld903@users.noreply.github.com>
2023-04-25 17:58:40 +0800

    Delete java/com/test directory

--- java/com/test/common/util/MailUtils.java
+++
@@ -1,9 +0,0 @@
-package com.test.common.util;
-
-public class MailUtils {
-	public static String randomNumBuilder() {
-		String result="";
-		for(int i=0;i<12;i++)result+=Math.round(Math.random()*9);
-		return result;
-	}
-}
